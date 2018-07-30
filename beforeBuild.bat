2512df76066aad187130995889d8ca2bdc453cb6
zhaoji <zhao_ji@126.com>
2018-07-30 11:15:39 +0800

    修改硬件版本号为V003
    修改硬件版本号为V003

--- beforeBuild.bat
+++ beforeBuild.bat
@@ -2,7 +2,7 @@ cd /d %~dp0
 cd.>HAL\COMMON\build_version.h
 set h=%time:~0,2%
 
-echo #define __CMIOT_VERSION__ "D5200 V002 Build%date:~0,4%%date:~5,2%%date:~8,2% Rel.%h: =0%%time:~3,2%%time:~6,2%">>HAL\COMMON\build_version.h
+echo #define __CMIOT_VERSION__ "D5200 V003 Build%date:~0,4%%date:~5,2%%date:~8,2% Rel.%h: =0%%time:~3,2%%time:~6,2%">>HAL\COMMON\build_version.h
 
 del Output\*.hex /s
 del Output\*.bin /s
